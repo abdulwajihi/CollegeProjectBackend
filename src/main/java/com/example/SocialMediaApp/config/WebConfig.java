45124c865706ae7853eb1b90c8b9816917fdf185
abdulwajih <abdulwajih.khan786@gmail.com>
2025-05-27 15:26:34 +0530

    cors config add

---
+++ src/main/java/com/example/SocialMediaApp/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.SocialMediaApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allows all endpoints
+                .allowedOrigins("http://localhost:5173/") // Allows your frontend origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+
+}
