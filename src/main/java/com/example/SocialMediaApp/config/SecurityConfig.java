c1754b84f155eff13e10f5fb6665bf5b992827e6
abdulwajih <abdulwajih.khan786@gmail.com>
2025-06-04 00:42:17 +0530

    add origin

--- src/main/java/com/example/SocialMediaApp/config/SecurityConfig.java
+++ src/main/java/com/example/SocialMediaApp/config/SecurityConfig.java
@@ -97,6 +97,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("https://pixcircle.onrender.com"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
